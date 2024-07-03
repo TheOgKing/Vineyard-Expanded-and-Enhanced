dac62957efc7e3364f6a0fa8b9a10e6b7c722607
TheKing <37515711+TheOgKing@users.noreply.github.com>
2024-07-03 14:16:09 -0700

    Delete scripts directory

--- scripts/main.js
+++
@@ -1,8 +0,0 @@
-function inject(file, tag) {
-	const node = document.getElementsByTagName(tag)[0];
-	const script = document.createElement('script');
-	script.setAttribute('type', 'module');
-	script.setAttribute('src', file);
-	node.appendChild(script);
-}
-inject(chrome.runtime.getURL('scripts/vineyard.js'), 'body');
